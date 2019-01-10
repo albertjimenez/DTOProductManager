d54df0efd171f45ea425c350ab4da91bb6c0e01b
Beruto <beruto000@gmail.com>
2019-01-10 12:06:17 +0100

    testing null on CompraBean - salb

--- src/main/java/model/Beans.kt
+++ src/main/java/model/Beans.kt
@@ -39,7 +39,7 @@ data class CofradiaBean(val idCofradia: Int = 0, val poblacion: String = "", val
                         val idZona: Int = 0)
 
 data class CompraBean(val idCompra: Int = 0, val fecha: Date = Date.from(Instant.now()), val idCofradia: Int = 0,
-                      val salb: String = "", val gastoSubasta: Float = 0f, val idZona: Int = 0,
+                      val salb: String? = "", val gastoSubasta: Float = 0f, val idZona: Int = 0,
                       val porteIncluido: Int = 0)
 
 data class FamiliaBean(val idFamilia: String = "", val familia: String = "", val cientifico: String = "",
